9f018f7b4e4df89aaab02d4dbcb0969796252abc
alan hermosilla <alan.hermosilla@academy.local>
2026-02-12 20:05:34 -0300

    raton se mueve en bucle y tablero se actualiza

--- minimax_lab.py
+++ minimax_lab.py
@@ -1,5 +1,8 @@
+import random
+
 def movimientos_posibles(posicion, tamaño):
     movimientos = []
+    
 
     fila = posicion[0]
     columna = posicion[1]
@@ -24,6 +27,30 @@ tablero = [
 
 tablero[0][0] = "G"
 tablero[4][4] = "R"
+raton = [4,4]
+for i in range(5):
+
+    movs = movimientos_posibles(raton, 5)
+    print("Movimientos posibles:", movs)
+
+    nuevo_mov = movs[0]
+    print("Movimiento elegido:", nuevo_mov)
+
+    tablero[raton[0]][raton[1]] = "."
+    raton = nuevo_mov
+    tablero[raton[0]][raton[1]] = "R"
+
+    for fila in tablero:
+        print(fila)
+    print()
+
+
+
+movs = movimientos_posibles(raton, 5)
+print(movs)
+nuevo_mov = random.choice(movs)
+print(nuevo_mov)
+
 
 print(movimientos_posibles([2,2], 5))
 
